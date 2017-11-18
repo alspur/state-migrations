1b8195f4090f54f6cf257f8a88828169787ab516
Alex Spurrier <alex.spurrier@gmail.com>
2017-11-18 10:02:58 -0500

    initial commit; scrape spreadsheets

---
+++ R/scrape.R
@@ -0,0 +1,38 @@
+# scrape.R
+
+# load ----------
+
+# load packages
+library(tidyverse)
+library(rvest)
+
+
+# scrape --------
+
+# get url for census page w/ migration flow data
+census_url <- "https://www.census.gov/data/tables/time-series/demo/geographic-mobility/state-to-state-migration.html"
+
+# scrape webpage
+census_page <- read_html(census_url)
+
+# parse -------
+
+# get links to migration flow xls files
+migration_urls <- census_page %>% 
+  html_nodes("a.attachmentAnchor") %>% 
+  xml_attr("href")
+
+# create df of urls and file extensions for downloading
+migration_df <- tibble(url = migration_urls) %>% 
+  mutate(row_number = row_number(),
+         url = paste0("http:", url)) %>% 
+  select(row_number, url) %>% 
+  separate(url, into = c("front", "extension"), remove = FALSE,
+           sep = "/state-to-state-migration/") %>% 
+  mutate(extension= paste0("data/", tolower(extension)))
+
+# download --------
+
+# download files to /data 
+migration_df %>% 
+  walk(download.file(.$url, .$extension))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# state-migrations
-Tracking the flow of people from state to state
+# State Migrations
+
+Tracking the flow of people from state to state using data from the [American Community Survey](https://www.census.gov/data/tables/time-series/demo/geographic-mobility/state-to-state-migration.html).
